3bd061811d5b60bd18a8ab48ca6939d375d29556
Mike Lee <mikecovlee@163.com>
2024-09-08 18:11:18 +0800

    add broadcast algorithm

--- moe_peft/modules/config.py
+++ moe_peft/modules/config.py
@@ -220,8 +220,9 @@ class MixLoraConfig(LoraConfig):
     # mixtral config
     top_k_: int = None
     # dynamic config
+    broadcast_threshhold_: float = None
+    entropy_eps_: float = None
     top_p_: float = None
-    temperature_: float = None
     # switch transformers config
     router_z_loss_coef_: float = None
     expert_capacity_: int = None
@@ -253,9 +254,13 @@ class MixLoraConfig(LoraConfig):
             assert isinstance(self.top_k_, int) and self.top_k_ > 0
         elif self.routing_strategy_ == "mixlora-dynamic":
             assert (
+                isinstance(self.broadcast_threshhold_, float)
+                and self.broadcast_threshhold_ > 0
+            )
+            assert isinstance(self.entropy_eps_, float) and self.entropy_eps_ > 0
+            assert (
                 isinstance(self.top_p_, float) and self.top_p_ > 0 and self.top_p_ <= 1
             )
-            assert isinstance(self.temperature_, float) and self.temperature_ >= 0
         elif self.routing_strategy_ == "mixlora-switch":
             assert (
                 isinstance(self.router_z_loss_coef_, float)
@@ -291,8 +296,9 @@ class MixLoraConfig(LoraConfig):
         elif lora_config.routing_strategy_ == "mixlora-dynamic":
             lora_config.router_init_range_ = config.get("router_init_range", 0.02)
             lora_config.jitter_noise_ = config.get("jitter_noise", 0.0)
+            lora_config.broadcast_threshhold_ = config.get("broadcast_threshhold", 2.0)
+            lora_config.entropy_eps_ = config.get("entropy_eps", 1e-5)
             lora_config.top_p_ = config.get("top_p", 0.8)
-            lora_config.temperature_ = config.get("temperature", 0.0)
         elif lora_config.routing_strategy_ == "mixlora-switch":
             lora_config.router_init_range_ = config.get("router_init_range", 1.0)
             lora_config.jitter_noise_ = config.get("jitter_noise", 0.01)
@@ -322,8 +328,9 @@ class MixLoraConfig(LoraConfig):
         if self.routing_strategy_ == "mixlora":
             config["top_k"] = self.top_k_
         elif self.routing_strategy_ == "mixlora-dynamic":
+            config["broadcast_threshhold"] = self.broadcast_threshhold_
+            config["entropy_eps"] = self.entropy_eps_
             config["top_p"] = self.top_p_
-            config["temperature"] = self.temperature_
         elif self.routing_strategy_ == "mixlora-switch":
             config["expert_capacity"] = self.expert_capacity_
             config["sparse_step"] = self.sparse_step_
--- moe_peft/modules/mix_lora.py
+++ moe_peft/modules/mix_lora.py
@@ -263,29 +263,47 @@ class MixtralSparseMoe(LLMMoeBlock):
         return final_hidden_states, router_logits
 
 
-def _dynamic_top_p(router_logits: torch.Tensor, top_p: float, temperature: float = 0.0):
-    if temperature > 0.0:
-        router_logits = router_logits / temperature
+def _dynamic_routing(
+    router_logits: torch.Tensor,
+    broadcast_threshhold: float = 2.0,
+    top_p: float = 0.8,
+    eps: float = 1e-5,
+):
+    # calculate router entropy
+    probs_neg_log = -torch.log(router_logits + eps)  # eps for 'p=0, -plogp=0'
+    router_entropy = (router_logits * probs_neg_log).sum(dim=-1)
+    # broadcast if higher than threshhold
+    broadcast_index = router_entropy >= broadcast_threshhold
+    # calculate top-p routing
     sorted_logits, sorted_indices = torch.sort(router_logits, dim=-1, descending=True)
     cumulative_probs = sorted_logits.cumsum(dim=-1)
     expert_mask = cumulative_probs > top_p
+    # maintain top-1 if no experts selected
     threshold_indices = expert_mask.long().argmax(dim=-1)
     threshold_mask = torch.nn.functional.one_hot(
         threshold_indices, num_classes=sorted_indices.size(-1)
     ).bool()
+    # calculate final mask
     expert_mask = expert_mask & ~threshold_mask
+    expert_mask[broadcast_index] = False
     sorted_logits = sorted_logits.masked_fill(expert_mask, 0.0)
     sorted_indices = sorted_indices.masked_fill(expert_mask, -1)
-    return sorted_logits, sorted_indices
+    return router_entropy, sorted_logits, sorted_indices
 
 
 def _dynamic_load_balancing_loss_func(
     routing_weights: torch.Tensor,
-    num_experts: int,
-    top_p: float,
-    temperature: float,
+    broadcast_threshhold: float = 2.0,
+    top_p: float = 0.8,
+    eps: float = 1e-5,
 ) -> float:
-    _, selected_experts = _dynamic_top_p(routing_weights, top_p, temperature)
+    num_experts = routing_weights.size(-1)
+
+    router_entropy, _, selected_experts = _dynamic_routing(
+        routing_weights, broadcast_threshhold, top_p, eps
+    )
+
+    entropy_loss = torch.mean(router_entropy, dim=0)
 
     expert_mask = torch.empty(
         (num_experts, num_experts, routing_weights.size(0)),
@@ -305,25 +323,25 @@ def _dynamic_load_balancing_loss_func(
     router_prob_per_expert = torch.mean(routing_weights, dim=0)
 
     overall_loss = torch.sum(tokens_per_expert * router_prob_per_expert.unsqueeze(0))
-    return overall_loss * num_experts
+    return entropy_loss + overall_loss * num_experts
 
 
 class DynamicRouterLoss(torch.nn.Module):
     def __init__(self, config: MixLoraConfig) -> None:
         super().__init__()
         self.aux_loss_coef = config.router_aux_loss_coef_
-        self.experts = config.num_experts_
-        self.top_p = config.top_p_
-        self.temperature = config.temperature_
+        self.broadcast_threshhold: float = config.broadcast_threshhold_
+        self.top_p: float = config.top_p_
+        self.eps: float = config.entropy_eps_
 
     def forward(self, gate_logits, attention_mask) -> torch.Tensor:
         concatenated_gate_logits = _unpack_router_logits(gate_logits)
         routing_weights = torch.nn.functional.softmax(concatenated_gate_logits, dim=-1)
         return self.aux_loss_coef * _dynamic_load_balancing_loss_func(
             routing_weights,
-            self.experts,
+            self.broadcast_threshhold,
             self.top_p,
-            self.temperature,
+            self.eps,
         )
 
 
@@ -352,9 +370,10 @@ class DynamicSparseMoe(LLMMoeBlock):
             else config.act_fn_
         )
         self.experts_: int = config.num_experts_
-        self.top_p_: float = config.top_p_
-        self.temperature_: float = config.temperature_
         self.jitter_noise_: float = config.jitter_noise_
+        self.broadcast_threshhold_: float = config.broadcast_threshhold_
+        self.top_p_: float = config.top_p_
+        self.eps_: float = config.entropy_eps_
         self.router_profile_: bool = False
         self.profiler_: List[int] = None
 
@@ -413,11 +432,16 @@ class DynamicSparseMoe(LLMMoeBlock):
         router_logits = self.gate_(hidden_states)
 
         routing_weights = F.softmax(router_logits, dim=1, dtype=self.dtype_)
-        routing_weights, selected_experts = _dynamic_top_p(
-            routing_weights, self.top_p_, self.temperature_
+        _, routing_weights, selected_experts = _dynamic_routing(
+            routing_weights,
+            self.broadcast_threshhold_,
+            self.top_p_,
+            self.eps_,
         )
 
         self._profiling(batch_size, sequence_length, selected_experts)
+
+        routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
 
         final_hidden_states = torch.zeros(
             (batch_size * sequence_length, hidden_dim),
